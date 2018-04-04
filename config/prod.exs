46ff09c25b0e4027b1623c08f29a130aaa9cdd95
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-04-04 15:00:48 -0400

    remove static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :agent_desktop, AgentDesktop.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
 config :agent_desktop,
